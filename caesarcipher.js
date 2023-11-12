a9d52ad12a2e81c36ca9843eb51a6ec20e09cb18
Vikram Thangavel <76668807+VikramT19@users.noreply.github.com>
2023-11-12 15:55:30 -0500

    Create caesarcipher.js

---
+++ caesarcipher.js
@@ -0,0 +1,8 @@
+function rot13(str) {
+    return str.replace(/[A-Z]/g, (char) => {
+        // Determine the decoded letter using ROT13 algorithm
+        const decodedChar = String.fromCharCode((char.charCodeAt(0) - 65 + 13) % 26 + 65);
+        return decodedChar;
+    });
+}
+rot13("SERR PBQR PNZC");
